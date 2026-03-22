8fb6f48c9e8b34b43e1dac0f33efc63da18962c5
symphony407 <velozsymphony@gmail.com>
2026-03-21 20:52:17 -0500

    Update html with form;placeholeders for author, title, and pages; fix js errors;

--- library.js
+++ library.js
@@ -6,6 +6,7 @@ constructor(title, author,amountOfPages, _read) {
 this.title = title;
 this.author = author;
 this.amountOfPages = amountOfPages;
+this.read = _read;
     } 
 toggleRead() {
     this.read = !this.read;
@@ -19,21 +20,38 @@ function addBookToLibrary(title, author, amountOfPages, _read) {
     displayBooks();
 }
 
-addBookToLibrary("Moby Dick" , "Herman Melville" , 300, false);
+const form = document.getElementById("book-form");
+form.addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    const title = document.getElementById("title").value;
+    const author = document.getElementById("author").value;
+    const amountOfPages = document.getElementById("pages").value;
+    const read = document.getElementById("read").checked;
+
+    addBookToLibrary(title, author, amountOfPages, read);
+});
+
+
 
 function displayBooks() {
     container.textContent = "";
     myLibrary.forEach(function(book) {
         const card = document.createElement("div");
+        card.classList.add("book-card");
+
         const titleElement = document.createElement("span");
         titleElement.textContent = book.title;
         card.append(titleElement);
+
         const authorElement = document.createElement("p");
         authorElement.textContent = "Author: " + book.author;
         card.append(authorElement);
+
         const pagesElement = document.createElement("p");
         pagesElement.textContent = "Pages: " + book.amountOfPages;
         card.append(pagesElement);
+
         const readElement = document.createElement("p");
         let status = book.read ? "Yes" : "No";
         readElement.textContent = "Read: " + status;
@@ -43,8 +61,21 @@ function displayBooks() {
         const toggleButton = document.createElement("button")
         let buttonText = book.read ? "Mark as Unread" : "Mark as Read";
         toggleButton.textContent = buttonText;
-        card.append(toggleaButton);
-        
+        toggleButton.addEventListener("click", () => {
+            book.toggleRead();
+            displayBooks();
+        })
+        card.append(toggleButton);
+
+        const deleteButton = document.createElement("button");
+        deleteButton.textContent = "Delete";
+        deleteButton.addEventListener("click", () => {
+            myLibrary.splice(myLibrary.indexOf(book), 1);
+            displayBooks();
+        });
+        card.append(deleteButton);
+
+        container.append(card);
     });
 }
 
